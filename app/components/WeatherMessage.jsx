bd0ad71fd7c111df681c6b2835124b68657c0a46
Tae Hoon Lee <thlee1122@hotmail.com>
2017-04-09 22:43:35 -0400

    Initial Commit

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+
+// var WeatherMessage = React.createClass({
+	
+//   	render: function () {
+//   		var {temp, location} = this.props;
+
+//   		return (
+//   			<div>
+// 	  			<h3>It's is {temp} in {location}.</h3>
+
+//   			</div>
+//   		)
+//   	}
+// });
+
+var WeatherMessage = ({temp, location}) => {
+
+	return (
+		<h3>It's is {temp} in {location}.</h3>
+	)
+
+}
+
+module.exports = WeatherMessage;
